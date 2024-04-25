3131e726356e6065c6442fd97c2ba058f60562ef
AOFNH <bellamy_n_h_chief@163.com>
2024-04-25 18:10:51 +0800

    feat: 修改 MacOS 版

--- README.md
+++ README.md
@@ -3,20 +3,26 @@
 在 Windows、MacOS 和 Linux 系统中，JetBrains IDE 使用以下目录来存储其配置文件、缓存、插件和日志：  
 
 Windows:
+
 + 配置文件 (Config): `%APPDATA%\JetBrains\IntelliJIdea<version>`
 + 缓存 (System & Logs): `%LOCALAPPDATA%\JetBrains\IntelliJIdea<version>`
 + 插件 (Plugins): `%APPDATA%\JetBrains\IntelliJIdea<version>\plugins`
 + 日志文件 (Logs): `%LOCALAPPDATA%\JetBrains\IntelliJIdea<version>\log`
 
 MacOS:
+
 + 配置文件 (Config): `~/Library/Application Support/JetBrains/IntelliJIdea<version>`
 + 缓存 (System & Logs): `~/Library/Caches/JetBrains/IntelliJIdea<version>`
 + 插件 (Plugins): `~/Library/Application Support/JetBrains/IntelliJIdea<version>/plugins`
 + 日志文件 (Logs): `~/Library/Logs/JetBrains/IntelliJIdea<version>`
 
 Linux:
+
 + 配置文件 (Config): `~/.config/JetBrains/IntelliJIdea<version>`
 + 缓存 (System & Logs): `~/.cache/JetBrains/IntelliJIdea<version>`
 + 插件 (Plugins): `~/.local/share/JetBrains/IntelliJIdea<version>/plugins`
 + 日志文件 (Logs): `~/.cache/JetBrains/IntelliJIdea<version>/log`
-  
+
+[Directories used by the IDE](https://www.jetbrains.com/help/idea/directories-used-by-the-ide-to-store-settings-caches-plugins-and-logs.html)  
+[Toolbox App 1.22 – It’s Time to Clean up Some Disk Space!](https://blog.jetbrains.com/blog/2021/11/09/toolbox-app-1-22/)  
+[Cleanup unused IDE directories](https://www.jetbrains.com/guide/go/tips/cleanup-unused-ide-directories/)  
--- Remove-All-OldJetBrainsVersions.sh
+++ Remove-All-OldJetBrainsVersions.sh
@@ -1,62 +1,63 @@
 #!/bin/bash
 
 # Define the paths to JetBrains folders in Application Support, Caches, and Logs
-paths=(
-  "$HOME/Library/Application Support/JetBrains"
-  "$HOME/Library/Caches/JetBrains"
-  "$HOME/Library/Logs/JetBrains"
-)
-
-# Initialize a associative array to track found software
-declare -A foundSoftware
+config_path="$HOME/Library/Application Support/JetBrains"
+cache_path="$HOME/Library/Caches/JetBrains"
+logs_path="$HOME/Library/Logs/JetBrains"
 
 # Ask the user for software list input
-read -p "Enter the software list separated by commas (e.g., 'WebStorm,DataGrip,IntelliJIdea'), or press Enter to use default values: " userInput
+read -p "Enter the software list separated by commas (e.g., 'WebStorm,DataGrip,IntelliJIdea'), or press Enter to use default values: " user_input
 
 # If the user input is empty, use the default software list
-if [[ -z "$userInput" ]]; then
-  softwareList=("WebStorm" "DataGrip" "IntelliJIdea")
+if [ -z "$user_input" ]; then
+    software_list=("WebStorm" "DataGrip" "IntelliJIdea")
 else
-  IFS=',' read -r -a softwareList <<< "$userInput"
+    IFS=',' read -ra software_list <<< "$user_input"
 fi
 
 # Initialize foundSoftware with all software set to false
-for software in "${softwareList[@]}"; do
-  foundSoftware[$software]=false
+declare -A foundSoftware
+for software in "${software_list[@]}"; do
+    foundSoftware[$software]=false
 done
 
-# Process each specified JetBrains path
-for path in "${paths[@]}"; do
-  if [[ -d "$path" ]]; then
-    for software in "${softwareList[@]}"; do
-      directories=($(find "$path" -type d -name "$software*"))
-      if [[ ${#directories[@]} -gt 0 ]]; then
-        foundSoftware[$software]=true
-        # Sort the directories to find the oldest ones (assuming version format)
-        IFS=$'\n' sortedDirectories=($(sort -V <<<"${directories[*]}"))
-        unset IFS
-        # We skip the last (newest) directory and consider old ones for removal
-        directoriesToRemove=("${sortedDirectories[@]:0:${#sortedDirectories[@]}-1}")
-        # Interact with the user for each directory before removing it
-        for dirToRemove in "${directoriesToRemove[@]}"; do
-          read -p "Do you want to remove the directory $dirToRemove? [Y/N]: " userConfirmation
-          if [[ "$userConfirmation" == "Y" ]]; then
-            # rm -rf "$dirToRemove"
-            echo "Removed directory: $dirToRemove"
-          else
-            echo "Skipped directory: $dirToRemove"
-          fi
-        done
-      fi
+# Function to process JetBrains directories
+process_directories() {
+    for software in "${software_list[@]}"; do
+        dirs_to_remove=()
+
+        # Find directories matching software and version pattern
+        directories=$(find "$1" -maxdepth 1 -mindepth 1 -type d -name "$software*" | sort -rV)
+
+        if [[ $directories ]]; then
+            foundSoftware[$software]=true
+
+            # Read all but the newest directory into an array
+            read -ra dirs_to_remove <<< "$(echo "$directories" | sed -n '1!p')"
+
+            for dirToRemove in "${dirs_to_remove[@]}"; do
+                read -p "Do you want to remove the directory $dirToRemove? [Y/N] " user_confirmation
+
+                if [ "$user_confirmation" == "Y" ] || [ "$user_confirmation" == "y" ]; then
+                    # Remove directory
+                    # rm -rf "$dirToRemove"
+                    echo "Removed directory: $dirToRemove"
+                else
+                    echo "Skipped directory: $dirToRemove"
+                fi
+            done
+        fi
     done
-  else
-    echo "Path not found: $path"
-  fi
-done
+}
+
+# Process directories in Application Support, Caches, and Logs
+process_directories "$config_path"
+process_directories "$cache_path"
+process_directories "$logs_path"
 
 # Check if any software was not found in any of the paths
-for software in "${softwareList[@]}"; do
-  if [[ ${foundSoftware[$software]} == false ]]; then
-    echo "No directories found for software: $software"
-  fi
+for software in "${software_list[@]}"; do
+    if [ "${foundSoftware[$software]}" == false ]; then
+        echo "No directories found for software: $software"
+    fi
 done
